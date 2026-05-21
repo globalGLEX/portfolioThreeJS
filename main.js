6906345be4f0c6c538c4096dfbe7be3396ee4fee
globalGLEX <priitkru@gmail.com>
2026-05-21 16:02:01 +0300

    add audio features

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ import { Water } from 'three/examples/jsm/objects/Water.js';
 import { TextureLoader } from 'three';
 import './main.css';
 
-
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set( 0.43, 2.66, 19);
@@ -28,89 +26,16 @@ controls.target.set(0, 0, 0);
 controls.update();
  */
 
-/* // large flat plane just beyond water that matches fog color
-const mistGeometry = new THREE.PlaneGeometry(100, 100);
-const mistMaterial = new THREE.MeshBasicMaterial({ 
-  color: 0xc9d8e0,
-  transparent: true,
-  opacity: 0.6,
-});
-const mist = new THREE.Mesh(mistGeometry, mistMaterial);
-//mist.rotation.x = -Math.PI / 2;
-mist.position.y = 0.01; // just above water
-mist.position.z = -25; // just above water
-scene.add(mist); */
-/* const planeSize = 40;
-     
-const loader = new THREE.TextureLoader();
-const texture = loader.load('https://threejs.org/manual/examples/resources/images/checker.png');
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.magFilter = THREE.NearestFilter;
-texture.colorSpace = THREE.SRGBColorSpace;
-const repeats = planeSize / 2;
-texture.repeat.set(repeats, repeats);
-
-const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-const planeMat = new THREE.MeshPhongMaterial({
-  map: texture,
-  side: THREE.DoubleSide,
-});
-const mesh = new THREE.Mesh(planeGeo, planeMat);
-mesh.rotation.x = Math.PI * -.5;
-scene.add(mesh); */
-// use CubeCamera positioned at water level
-// replace your water mesh with Reflector
-
-
-
-
-/* const waterGeometry = new THREE.PlaneGeometry(20, 20, 50, 50);
-const waterMaterial = new THREE.MeshPhysicalMaterial({
-    color: 0x006994,
-    metalness: 0.1,
-    roughness: 0.0,      // 0 = mirror-like
-    transmission: 0.5,   // transparency
-    thickness: 0.5,
-    envMapIntensity: 1,  // reflection strength
-    side: THREE.DoubleSide,
-});
-
-const water = new THREE.Mesh(waterGeometry, waterMaterial);
-water.rotation.x = -Math.PI / 2; // makes it lay flat
-water.position.y = 0; // adjust height */
-
-/* water.material = new THREE.MeshBasicMaterial({ 
-    color: 0xff0000, 
-    side: THREE.DoubleSide,
-    wireframe: true  // wireframe so you can see it even if it overlaps something
-  }); */
-
-//water.scale.set(10, 10, 10);
-
-
-/* console.log(water.rotation);
-console.log(water.geometry.parameters);
-console.log(water.geometry.attributes.position.count);  */
-/* scene.add(water); */
-
-/* const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512);
-const cubeCamera = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget);
-cubeCamera.position.set(0, 0.1, 0);
-scene.add(cubeCamera); */
-
 const loader = new GLTFLoader();
 const textureLoader = new THREE.TextureLoader();
 const diffuse = textureLoader.load('tallgrass/textures/M_FoliageAtlas2_diffuse.png');
 diffuse.colorSpace = THREE.SRGBColorSpace;
 diffuse.flipY = false;
 
-
 loader.load('dock/scene.gltf', (gltf) => {
 
   const dock = gltf.scene;
   dock.position.set(-6, -2, 20);
-  //dock.rotateY(180);
   scene.add(gltf.scene);
   onModelLoaded();
 });
@@ -131,14 +56,14 @@ positions.needsUpdate = true;
 geometry.computeVertexNormals(); */
 
 const material = new THREE.MeshPhysicalMaterial({
-  color: 0x00aa00,      // green
+  color: 0xF59527,      // main color
   metalness: 0.9,
   roughness: 0.1,
   reflectivity: 1.0,
   clearcoat: 1.0,       // adds extra glossy top layer
   clearcoatRoughness: 0.1,
   sheen: 0.5,           // gives it a slight fabric/foil sheen
-  sheenColor: 0x00ff00, // sheen highlight color
+  sheenColor: 0xF59527, // sheen highlight color
 });
 let modelsLoaded = 0;
 const totalModels = 3; 
@@ -147,6 +72,8 @@ function onModelLoaded() {
   modelsLoaded++;
   if (modelsLoaded === totalModels) {
     document.getElementById('moon-container').style.display = 'block';
+    document.getElementById('volume').style.display = 'block';
+    document.getElementById('sound-btn').style.display = 'block';
     startPlaneAnimation();
   }
 }
@@ -162,7 +89,7 @@ plane.visible = true;
 gsap.to(plane.position, {
   y: 4,
   duration: 2.5,
-  ease: 'expo.out', // easing in from fast to slow
+  ease: 'back.out', // easing in from fast to slow
   delay: 2,         // optional delay before starting
 });
 }
@@ -330,33 +257,6 @@ loader.load(
   scene.add(gltf.scene);
   onModelLoaded();//for waiting before plane animation
 
-    /* const model1 = gltf.scene.clone();
-    model1.position.set(1, -0.5, 0);
-    model1.rotateY(95);
-    scene.add(model1);
-    const model2 = gltf.scene.clone();
-    model2.position.set(2, 0, 0);
-    scene.add(model2);
-    const model3 = gltf.scene.clone();
-    model3.position.set(3, 0, 0);
-    model3.rotateY(165);
-    scene.add(model3);
-    const model4 = gltf.scene.clone();
-    model4.position.set(4, 0, 0);
-    model4.rotateY(45);
-    scene.add(model4);
-    const model5 = gltf.scene.clone();
-    model5.position.set(5, 0, 0);
-    model5.rotateY(200);
-    scene.add(model5);
-    const model6 = gltf.scene.clone();
-    model6.position.set(5, 0, -5);
-    model6.rotateY(100);
-    scene.add(model6); */
-
-
-    
-
   },
   (progress) => {
     console.log('loading...', (progress.loaded / progress.total * 100) + '%');
@@ -375,38 +275,7 @@ const skyMaterial = new THREE.MeshBasicMaterial({
 });
 const sky = new THREE.Mesh(skyGeometry, skyMaterial);
 scene.add(sky);
-/* const geometry2 = new THREE.BufferGeometry(); // your model geometry
-const material2 = new THREE.MeshStandardMaterial();
-const count2 = 100;
 
-const instancedMesh = new THREE.InstancedMesh(geometry2, material2, count2);
-
-const matrix = new THREE.Matrix4();
-for (let i = 0; i < count; i++) {
-  matrix.setPosition(x, y, z); // set each instance position
-  instancedMesh.setMatrixAt(i, matrix);
-}
-
-scene.add(instancedMesh); */
-
-
-/* const waterGeometry = new THREE.PlaneGeometry(100, 100, 50, 50);
-const water = new Reflector(waterGeometry, {
-  clipBias: 0.003,
-  textureWidth: window.innerWidth * window.devicePixelRatio,
-  textureHeight: window.innerHeight * window.devicePixelRatio,
-  color: 0x006994,
-});
-water.material = new THREE.MeshStandardMaterial({
-    color: 0x006994,
-    envMap: cubeRenderTarget.texture,
-    envMapIntensity: 0.3,
-    roughness: 0.1,
-    metalness: 0.9,
-  });
-  
-water.rotation.x = -Math.PI / 2;
-scene.add(water); */
 const waterGeometry = new THREE.PlaneGeometry(100, 100);
 
 const water = new Water(waterGeometry, {
@@ -424,31 +293,13 @@ const water = new Water(waterGeometry, {
 water.rotation.x = -Math.PI / 2;
 scene.add(water);
 
-/* {
-    const cubeSize = 4;
-    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-    const cubeMat = new THREE.MeshPhongMaterial({color: '#8AC'});
-    const mesh = new THREE.Mesh(cubeGeo, cubeMat);
-    mesh.position.set(cubeSize + 1, cubeSize / 2, 0);
-    scene.add(mesh);
-  }
-  {
-    const sphereRadius = 3;
-    const sphereWidthDivisions = 32;
-    const sphereHeightDivisions = 16;
-    const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions);
-    const sphereMat = new THREE.MeshPhongMaterial({color: '#CA8'});
-    const mesh = new THREE.Mesh(sphereGeo, sphereMat);
-    mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0);
-    scene.add(mesh);
-  } */
 
-  const color = 0xFFFFFF;
-  const intensity = 80;
-  const light = new THREE.PointLight(color, intensity);
-  light.position.set(1.4, 2.75, 0.5);
+const color = 0xFFFFFF;
+const intensity = 80;
+const light = new THREE.PointLight(color, intensity);
+light.position.set(1.4, 2.75, 0.5);
   
-  scene.add(light);
+scene.add(light);
   
 /*   const spotLight = new THREE.SpotLight(0xffffff, 100);
 spotLight.position.set(10, 5, 0);
@@ -457,23 +308,61 @@ spotLight.penumbra = 0.3;       // soft edges
 scene.add(spotLight);
  */
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 9.5);
-  directionalLight.intensity = 9.5; 
-  directionalLight.position.set(0, 10, 20);
-  scene.add(directionalLight);
+// Three.js audio setup
+let audioStarted = false; 
+const listener = new THREE.AudioListener();
+camera.add(listener);
 
-  const helper = new THREE.PointLightHelper(light);
-  scene.add(helper);
+const sound = new THREE.Audio(listener);
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load('audio.mp3', (buffer) => {
+  sound.setBuffer(buffer);
+  sound.setLoop(true);
+  sound.setVolume(0.3);
+  //sound.play();
+});
+const soundBtn = document.getElementById('sound-btn');
 
-  function makeXYZGUI(gui, vector3, name, onChangeFn) {
+soundBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  if (listener.context.state === 'suspended') {
+    listener.context.resume();
+  }
+  if (!audioStarted) {
+    audioStarted = true;
+    sound.play();
+    soundBtn.textContent = '🔊';
+  } else if (sound.isPlaying) {
+    sound.pause();
+    soundBtn.textContent = '🔇';
+  } else {
+    sound.play();
+    soundBtn.textContent = '🔊';
+  }
+});
+
+// connect slider to volume
+document.getElementById('volume').addEventListener('input', (e) => {
+  sound.setVolume(e.target.value);
+});
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 9.5);
+directionalLight.intensity = 9.5; 
+directionalLight.position.set(0, 10, 20);
+scene.add(directionalLight);
+
+const helper = new THREE.PointLightHelper(light);
+scene.add(helper);
+
+function makeXYZGUI(gui, vector3, name, onChangeFn) {
     const folder = gui.addFolder(name);
     folder.add(vector3, 'x', -20, 20).onChange(onChangeFn);
     folder.add(vector3, 'y', -10, 20).onChange(onChangeFn);
     folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
     folder.open();
-  }
+}
   
-  class ColorGUIHelper {
+class ColorGUIHelper {
       constructor(object, prop) {
         this.object = object;
         this.prop = prop;
@@ -484,15 +373,15 @@ scene.add(spotLight);
       set value(hexString) {
         this.object[this.prop].set(hexString);
       }
-    }
+}
 
 function updateLight() {
     
     helper.update();
-    }
+}
 
-  const gui = new GUI();
-  gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
+const gui = new GUI();
+gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
 gui.add(light, 'intensity', 0, 250, 1);
 gui.add(light, 'distance', 0, 40).onChange(updateLight);
 /* gui.add(light.target.position, 'x', -10, 10);
@@ -500,9 +389,6 @@ gui.add(light.target.position, 'z', -10, 10);
 gui.add(light.target.position, 'y', 0, 10); */
 
 makeXYZGUI(gui, light.position, 'position', updateLight);
-/* makeXYZGUI(gui, light.target.position, 'target', updateLight); */
-
-//camera.position.z = 22;
 
 const timer = new THREE.Timer();
 
@@ -518,17 +404,7 @@ function animate() { //used to have (time)
     
     timer.update();
 
-    // sync cubeCamera position to model each frame
-   /*  if (tallgrass) { // check it's loaded before using
-        cubeCamera.position.x = tallgrass.position.x;
-        cubeCamera.position.z = tallgrass.position.z;
-        cubeCamera.position.y = 0.1;
-        
-      } */
-
-    water.visible = false;      // hide water so it doesn't reflect itself
- // cubeCamera.update(renderer, scene);
-  water.visible = true;
+    water.visible = true;
     const time = timer.getElapsed();
     const positions = water.geometry.attributes.position;
     for (let i = 0; i < positions.count; i++) {
@@ -546,7 +422,7 @@ function animate() { //used to have (time)
     positions.needsUpdate = true;
     water.geometry.computeVertexNormals();
       
-    water.material.uniforms['time'].value += 0.002;
+    water.material.uniforms['time'].value += 0.002; //water speed
     
     
     renderer.render( scene, camera );
